c4691a387e18c38fbeea3c0165009d002eb928ff
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-01 00:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+describe("Image tests", () => {
+
+  it("checks image max-width", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.get("main.cards img").each(($img) => {
+      cy.wrap($img).should("have.css", "max-width", "250px");
+    });
+  });
+
+});
+
